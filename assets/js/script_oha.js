d0a764c2f4736e04ab12168f8c9f872ae330ccb6
zenchi36 <h.zenkei@gmail.com>
2025-08-29 01:40:20 +0900

    Update script_oha.js

--- assets/js/script_oha.js
+++ assets/js/script_oha.js
@@ -90,6 +90,7 @@ function generateCalendar(startYear, startMonth) {
 
 
 const oha_date = [
+  { odate: "20250828", otime: "1953", id: "1961019347473973545",icon:"🔆" },
   { odate: "20250827", otime: "1206", id: "1960539491955077556",icon:"🎂" },
   { odate: "20250826", otime: "0710", id: "1960102570699211213",icon:"🔆" },
   { odate: "20250825", otime: "0631", id: "1959730393357410395",icon:"🔆" },
