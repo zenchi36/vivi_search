92f36e1329143f93fb5da858b9cff47b6310f31b
zenchi36 <h.zenkei@gmail.com>
2025-09-03 23:55:55 +0900

    Add files via upload

--- assets/js/script_oha.js
+++ assets/js/script_oha.js
@@ -90,6 +90,7 @@ function generateCalendar(startYear, startMonth) {
 
 
 const oha_date = [
+  { odate: "20250903", otime: "1945", id: "1963191541511618634",icon:"🔆" },
   { odate: "20250902", otime: "1918", id: "1962822469053751302",icon:"🔆" },
   { odate: "20250901", otime: "1918", id: "1962460153435005055",icon:"🔆" },
   { odate: "20250831", otime: "1203", id: "1961987848699273427",icon:"💃" },
